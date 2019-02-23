882f429c607a7c5fa560fce150c5e9e37f7dccdb
w3bgir1 <akulshyna@gmail.com>
2019-02-23 16:10:01 +0100

    done

---
+++ src/async-functions.js
@@ -0,0 +1,20 @@
+const giveItBackLater = (value, callback) => {
+    setTimeout(() => callback(value), 1000)
+
+}
+
+const addSomePromises = (somePromise) => {
+    return somePromise
+    .then(
+        result => result.repeat(2),
+        err => err.repeat(3)
+    )
+}
+
+const promiseToGiveItBackLater = value => {
+    return new Promise((resolve, reject) => {
+        giveItBackLater(value, () => resolve(value))
+  })
+}
+
+module.exports = { giveItBackLater, addSomePromises, promiseToGiveItBackLater }
